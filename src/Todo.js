9f6cbbc4d72fa2f1f6e337b7f5f887ce3cd6ba6e
DrYoussefTemraz <youseftahertemraz@gmail.com>
2023-12-27 22:16:34 +0200

    edit form with maintain the previous state

--- src/Todo.js
+++ src/Todo.js
@@ -3,19 +3,57 @@ import React, { Component } from "react";
 export default class Todo extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isEditing: false,
+      //add a task as state that we change with edit/update
+      task: this.props.task,
+    };
     this.handleRemove = this.handleRemove.bind(this);
+    this.toggeleForm = this.toggeleForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+
+  }
+  handleChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+  handleUpdate(evt){
+    evt.preventDefault();
+  }
+  //this toggle to make the boolean state to reverse
+  toggeleForm(){
+    this.setState({isEditing: !this.state.isEditing})
   }
   handleRemove() {
     this.props.removeTodo(this.props.id);
   }
   render() {
-    return (
-      <div>
-        <button>Edit</button>
-        <button onClick={this.handleRemove}>X</button>
+    let result;
+    //add editing condition
+    if (this.state.isEditing) {
+      result = (
+        <div>
+          <form onSubmit={this.handleUpdate}>
+            <input
+              type="text"
+              value={this.state.task}
+              name="task"
+              onChange={this.handleChange}
+            ></input>
+            <button>Update</button>
+          </form>
+        </div>
+      );
+    } else {
+      result = (
+        <div>
+          <button onClick={this.toggeleForm}>Edit</button>
+          <button onClick={this.handleRemove}>X</button>
 
-        <li>{this.props.task}</li>
-      </div>
-    );
+          <li>{this.props.task}</li>
+        </div>
+      );
+    }
+    return result;
   }
 }
